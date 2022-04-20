e82549f4b62fa0e487b92ef8775f9364dbb9ab99
Adrian Otalora <edotalora@gmail.com>
2022-04-20 11:46:53 -0500

    Viewers component added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
 function Home(props) {
   return (
     <Container>
       <ImgSlider></ImgSlider>
+      <Viewers></Viewers>
     </Container>
   );
 }
