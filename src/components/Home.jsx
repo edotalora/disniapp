a61e4987fee27dd32ad0af1d6d00cc4313640454
Adrian Otalora <edotalora@gmail.com>
2022-04-19 19:06:51 -0500

    slider added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
 function Home(props) {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <ImgSlider></ImgSlider>
+    </Container>
+  );
 }
 
 export default Home;
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh-70px);
+  padding: 0 calc(3.5vw + 5px);
+  height: 100vh;
+  overflow-x: hidden;
+  &:before {
+    position: absolute;
+    background-image: url("/images/home-background.png");
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    content: "";
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+`;
